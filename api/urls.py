1b22db6e783eae3ea49c62f1bdd3834edd7d43cb
medram <mramouchy@gmail.com>
2021-04-17 17:32:57 +0000

    fixed api Routing

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
-from rest_framework.urlpatterns import format_suffix_patterns
+#from rest_framework.urlpatterns import format_suffix_patterns
 #from rest_framework.authtoken import views
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 from django.urls import path, include
 from django.conf import settings
@@ -10,7 +10,7 @@ from . import views
 app_name = __package__
 
 
-router = routers.SimpleRouter(trailing_slash=True)
+router = routers.DefaultRouter(trailing_slash=True)
 router.register(r'contact', views.ContactViewSet)
 
 urlpatterns = [
@@ -18,13 +18,3 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
-
-
-# urlpatterns = [
-#     path('auth/', views.CustomAuthToken.as_view()),
-#     path('contact/', views.ContactList.as_view()),
-#     path('contact/<int:pk>', views.ContactDetail.as_view()),
-# 	  path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
