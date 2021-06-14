dc00a9b1574c5e5a679505df74bc331436da8521
medram <mramouchy@gmail.com>
2021-06-14 18:31:47 +0100

    added insights on home dashboard

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from sms.models import Contact, ContactList, SMS
+from bulk_manager.models import Campaign
 
-# admin.site.site_title = f'{settings.APP_NAME}'
-# admin.site.site_header = f'{settings.APP_NAME}'
+User = get_user_model()
 
 
 class MyAdminSite(admin.AdminSite):
     index_title = 'Dashboard'
     #enable_nav_sidebar = False
+
+    def index(self, request, extra_context=None):
+        if extra_context is None:
+            extra_context = {}
+
+        # Adding my context here
+        extra_context.update({
+            'insight': {
+                'users': User.objects.count(),
+                'contact_lists': ContactList.objects.count(),
+                'contacts': Contact.objects.count(),
+                'sms': SMS.objects.count(),
+                'campaigns': Campaign.objects.count(),
+            }
+        })
+
+        return super().index(request, extra_context=extra_context)
